0f8ba38f7c99d4008b0b6fef42a80ef449c7acca
Fengyang Wang <fengyang.wang.0@gmail.com>
2017-04-10 16:36:08 -0400

    StaticSites improvements

--- src/StaticSites/StaticSites.jl
+++ src/StaticSites/StaticSites.jl
@@ -1,22 +1,61 @@
 module StaticSites
 
-export StaticSite, generate_page
+using ..Remark
+export StaticSite, generate_page, prepare
 
 struct StaticSite
+    source_root     :: String
+    target_root     :: String
+    static_root     :: String
     default_modules :: Vector{Module}
+    default_page    :: String
+
+    function StaticSite(;
+                        source_root="remark/",
+                        target_root="target/",
+                        static_root="static/",
+                        default_modules=[],
+                        default_page="main.rem")
+        new(source_root,
+            target_root,
+            static_root,
+            default_modules,
+            default_page)
+    end
+end
+
+"""
+Prepare the site for page generation by:
+
+ 1. Building a directory structure skeleton
+ 2. Copying static files
+"""
+function prepare(site::StaticSite)
+    mkpath(site.target_root)
+    for file in readdir(site.static_root)
+        info(file; prefix="COPYING: ")
+        cp(joinpath(site.static_root, file),
+           joinpath(site.target_root, file);
+           remove_destination=true)
+    end
 end
 
 function generate_page(site::StaticSite,
-                       root, page="remark/core.rem";
+                       destination, page=site.default_page;
                        data::Associative{Symbol}=Dict{Symbol}(),
                        modules=[])
     start = Dates.now()
-    info(isempty(root) ? "Index Page" : root; prefix="GENERATING: ")
+    info(isempty(destination) ? "Index Page" : destination;
+         prefix="GENERATING: ")
 
-    try mkdir("public/$root") end
-    data[:currentpage] = root
-    open("public/$root/index.html", "w") do f
-        Remarkable.Remark.tohtml(f, page, data;
+    location = joinpath(site.target_root, destination)
+    mkpath(location)
+
+    source = joinpath(site.source_root, page)
+
+    data[:currentpage] = destination
+    open(joinpath(location, "index.html"), "w") do f
+        Remark.tohtml(f, source, data;
                                  modules=vcat(site.default_modules, modules))
         println(f)
     end
